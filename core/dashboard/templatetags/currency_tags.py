5f1feb972d12f79447fc6f5d7fa9b41c83863b61
flyingdutchman <rede@asklora.ai>
2023-10-15 02:44:03 +0700

    aider: Updated currency function to add a space between the currency symbol and the amount.
    # Aider chat conversation:
    
    ASSISTANT: To add a space between the currency symbol and the amount, you can modify the `currency` function in the `currency_tags.py` file.
    
    Here's how you can do it:
    
    ```python
    core/dashboard/templatetags/currency_tags.py
    <<<<<<< HEAD
    def currency(value):
        locale.setlocale(locale.LC_ALL, "id_ID.UTF-8")
        return locale.currency(value, grouping=True)
    =======
    def currency(value):
        locale.setlocale(locale.LC_ALL, "id_ID.UTF-8")
        formatted_currency = locale.currency(value, grouping=True)
        return formatted_currency.replace("Rp", "Rp ")
    >>>>>>> updated
    ```
    
    This change will replace "Rp" with "Rp " (note the space after Rp), effectively adding a space between the currency symbol and the amount.

--- core/dashboard/templatetags/currency_tags.py
+++ core/dashboard/templatetags/currency_tags.py
@@ -7,4 +7,5 @@ register = template.Library()
 @register.filter(name="currency")
 def currency(value):
     locale.setlocale(locale.LC_ALL, "id_ID.UTF-8")
-    return locale.currency(value, grouping=True)
+    formatted_currency = locale.currency(value, grouping=True)
+    return formatted_currency.replace("Rp", "Rp ")
