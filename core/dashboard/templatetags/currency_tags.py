0522b3e22094256ff1657cee3a90d8fb30347668
flyingdutchman <rede@asklora.ai>
2023-10-15 02:39:20 +0700

    aider: Created Django template tags that translate an amount into Indonesian currency format.
    # Aider chat conversation:
    
    USER: create django template tags that receive amount, the template tag will translate it into indonesian currency format
    
    ASSISTANT: To create a Django template tag that translates an amount into Indonesian currency format, we need to create a new file in a Django app's templatetags directory. Let's create a new file named `currency_tags.py` in the `core/dashboard/templatetags` directory.
    
    Here's the code for the new template tag:
    
    ```python
    from django import template
    import locale
    
    register = template.Library()
    
    @register.filter(name='currency')
    def currency(value):
        locale.setlocale(locale.LC_ALL, 'id_ID.UTF-8')
        return locale.currency(value, grouping=True)
    ```
    
    This code creates a new template filter named `currency` that formats a number into the Indonesian currency format.
    
    You can use this template tag in your templates like this:
    
    ```html
    {{ amount|currency }}
    ```
    
    This will format the `amount` variable into the Indonesian currency format.
    
    Here's the *edit block* for the new file:
    
    ```python
    core/dashboard/templatetags/currency_tags.py
    <<<<<<< HEAD
    =======
    from django import template
    import locale
    
    register = template.Library()
    
    @register.filter(name='currency')
    def currency(value):
        locale.setlocale(locale.LC_ALL, 'id_ID.UTF-8')
        return locale.currency(value, grouping=True)
    >>>>>>> updated
    ```
    
    Please note that you need to have the `locale` module and the Indonesian locale installed on your system for this to work. If you don't have the Indonesian locale installed, you can install it using the following command on a Debian-based system:
    
    ```bash
    sudo apt-get install language-pack-id
    ```

---
+++ core/dashboard/templatetags/currency_tags.py
@@ -0,0 +1,9 @@
+from django import template
+import locale
+
+register = template.Library()
+
+@register.filter(name='currency')
+def currency(value):
+    locale.setlocale(locale.LC_ALL, 'id_ID.UTF-8')
+    return locale.currency(value, grouping=True)
