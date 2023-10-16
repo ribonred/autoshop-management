19c8e0ca3fff4acf1cbaf8c68e7996a57cf7b00d
flyingdutchman <rede@asklora.ai>
2023-10-16 22:18:49 +0700

    Added autocomplete functionality to the search input field using a predefined list of tags.

--- assets/static/js/main.js
+++ assets/static/js/main.js
@@ -2,5 +2,16 @@ window.onload = function () {
     document.body.addEventListener('htmx:configRequest', (event) => {
         event.detail.headers['X-CSRFToken'] = '{{ csrf_token }}';
     })
+
+    var availableTags = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        // Add more tags as needed
+    ];
+
+    $("#search-input").autocomplete({
+        source: availableTags
+    });
 }
 
